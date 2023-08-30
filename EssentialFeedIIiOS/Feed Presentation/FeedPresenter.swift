5044bdea700a0321d5f56115590675ba4f4b5d65
gtsofa <jlsnyule@yahoo.com>
2023-08-30 13:13:33 +0300

    Move from property injection to constructor injection in the `FeedPresenter` to guarantee its instances have access to its dependencies at all times.

--- EssentialFeedIIiOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedIIiOS/Feed Presentation/FeedPresenter.swift
@@ -26,20 +26,26 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-    var feedLoadingView: FeedLoadingView?
-    var feedView: FeedView?
+    // a presenter must have a view - our design needs refactoring!
+    private let feedView: FeedView
+    private let feedLoadingView: FeedLoadingView
+    
+    init(feedView: FeedView, feedLoadingView: FeedLoadingView) {
+        self.feedView = feedView
+        self.feedLoadingView = feedLoadingView
+    }
     
     func didStartLoadingFeed() {
-        feedLoadingView?.display(FeedLoadingViewModel(isLoading: true))
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
     func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView?.display(FeedViewModel(feed: feed))
-        feedLoadingView?.display(FeedLoadingViewModel(isLoading: false))
+        feedView.display(FeedViewModel(feed: feed))
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: false))
     }
     
     func didFinishLoading(with error: Error) {
-        feedLoadingView?.display(FeedLoadingViewModel(isLoading: false))
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: false))
     }
     
 }
--- EssentialFeedIIiOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeedIIiOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,12 +13,13 @@ public final class FeedUIComposer {
     public init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presenter = FeedPresenter()
-        let presentationAdapter = FeedLoadPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let presentationAdapter = FeedLoadPresentationAdapter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
-        presenter.feedLoadingView = WeakRefVirtualProxy(refreshController)
-        presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
+        presentationAdapter.presenter = FeedPresenter(
+            feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+            feedLoadingView: WeakRefVirtualProxy(refreshController))
+    
         return feedController
     }
     
@@ -67,23 +68,22 @@ private final class FeedViewAdapter: FeedView {
 private final class FeedLoadPresentationAdapter: FeedRefreshViewControllerDelegate {
     // has loader + presenter dependency
     private let feedLoader: FeedLoader
-    private let presenter: FeedPresenter
+    var  presenter: FeedPresenter?
     
-    init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+    init(feedLoader: FeedLoader) {
         self.feedLoader = feedLoader
-        self.presenter = presenter
     }
     
     // logic to communicate with feed component
     func didRequestFeedRefresh() {
-        presenter.didStartLoadingFeed()
+        presenter?.didStartLoadingFeed()
         
         feedLoader.load { [weak self] result in
             switch result {
             case let .success(feed):
-                self?.presenter.didFinishLoadingFeed(with: feed)
+                self?.presenter?.didFinishLoadingFeed(with: feed)
             case let .failure(error):
-                self?.presenter.didFinishLoading(with: error)
+                self?.presenter?.didFinishLoading(with: error)
             }
         }
     }
