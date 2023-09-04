f345a3a8d40b1c0996976b94d36f37331ac9073d
gtsofa <jlsnyule@yahoo.com>
2023-09-04 12:57:16 +0300

    Extract the `FeedViewController` creation and configuration into a factory method

--- EssentialFeedIIiOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeedIIiOS/Feed UI/Composers/FeedUIComposer.swift
@@ -14,18 +14,26 @@ public final class FeedUIComposer {
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoadPresentationAdapter(feedLoader: feedLoader)
+        let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
         
         
-        let bundle = Bundle(for: FeedViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController//FeedViewController(refreshController: refreshController)
-        //set the delegate direct w/o feedrefreshvc
-        feedController.delegate = presentationAdapter
-        feedController.title = FeedPresenter.title
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
             feedLoadingView: WeakRefVirtualProxy(feedController))
     
+        return feedController
+    }
+}
+
+extension FeedViewController {
+    static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController//FeedViewController(refreshController: refreshController)
+        //set the delegate direct w/o feedrefreshvc
+        feedController.delegate = delegate
+        feedController.title = FeedPresenter.title
+        
         return feedController
     }
 }
