c3790efbbc59cc84fb63cc0e539130649ff0e91c
gtsofa <jlsnyule@yahoo.com>
2023-09-15 07:43:23 +0300

    Extract refreshing logic to an extension

--- EssentialFeedIIiOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeedIIiOS/Feed UI/Controllers/FeedViewController.swift
@@ -41,11 +41,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
