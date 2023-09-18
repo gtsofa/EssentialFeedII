e7205431140ffd0189bc1a17a36e92278fcf5089
gtsofa <jlsnyule@yahoo.com>
2023-09-18 12:36:07 +0300

    Move `FeedErrorViewModel` to a separte file

---
+++ EssentialFeedII/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeedII
+//
+//  Created by Julius on 18/09/2023.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
--- EssentialFeedII/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedII/Feed Presentation/FeedPresenter.swift
@@ -29,18 +29,6 @@ public protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)// happens in the uirefreshcontrol
 }
 
-public struct FeedErrorViewModel {
-    public let message: String?
-    
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
