b4d928ceab96e5c7098b135261a21750107ea60b
gtsofa <jlsnyule@yahoo.com>
2023-09-19 14:17:08 +0300

    Make a `makeSUT()` factory method to prevent future breaking changes

--- EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -13,15 +13,22 @@ final class RemoteFeedImageDataLoader {
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_init_doesNotPerformAnyURLRequest() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertEqual(client.requestedURLs, [])
     }
     
     // MARK:- Helpers
     
-    class HTTPClientSpy {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy {
         var requestedURLs = [URL]()
     }
 
