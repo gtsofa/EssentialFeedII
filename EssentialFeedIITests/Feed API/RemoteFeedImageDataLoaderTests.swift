c77e2cf87bb432b564774ee52604da50c0197fcf
gtsofa <jlsnyule@yahoo.com>
2023-09-19 14:40:54 +0300

    `RemoteFeedImageDataLoader` requests data from URL on `loadImageDataFromURL`

--- EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -6,9 +6,18 @@
 //
 
 import XCTest
+import EssentialFeedII
 
 final class RemoteFeedImageDataLoader {
-    init(client: Any) {}
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
+    }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
@@ -18,9 +27,18 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [])
     }
     
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT()
+        
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     // MARK:- Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://any-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -28,8 +46,12 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private class HTTPClientSpy {
+    private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 
 }
