8c4f1d64d68bef2d4d357c4d44ca8c046ccdfa2a
gtsofa <jlsnyule@yahoo.com>
2023-09-21 13:14:02 +0300

    `RemoteFeedImageDataLoader` request data from URL on `loadImageDataFromURL`

--- EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,10 +9,14 @@ import XCTest
 import EssentialFeedII
 
 final class RemoteFeedImageDataLoader {
-    private let client: Any
+    private let client: HTTPClient
     
-    init(client: Any) {
+    init(client: HTTPClient) {
         self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
     }
 }
 
@@ -23,9 +27,23 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [])
     }
     
+    func test_loadImageData_requestDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     // MARK: - Helpers
     
-    class HTTPClientSpy {
-        let requestedURLs = [URL]()
+    class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 }
