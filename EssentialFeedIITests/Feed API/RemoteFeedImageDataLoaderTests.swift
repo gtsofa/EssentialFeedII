773c95e6e9b187033e4539c5e543630e22eb9943
gtsofa <jlsnyule@yahoo.com>
2023-09-21 12:54:49 +0300

    `RemoteFeedImageDataLoader` does not perform  any URL  request

--- EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,104 +9,23 @@ import XCTest
 import EssentialFeedII
 
 final class RemoteFeedImageDataLoader {
-    private let client: HTTPClient
+    private let client: Any
     
-    init(client: HTTPClient) {
+    init(client: Any) {
         self.client = client
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-        client.get(from: url) { result in
-            switch result {
-            case let .failure(error):
-                completion(.failure(error))
-            default:
-                break
-            }
-        }
     }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
-    func test_init_doesNotPerformAnyURLRequest() {
-        let (_, client) = makeSUT()
-        
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
         XCTAssertEqual(client.requestedURLs, [])
     }
     
-    func test_loadImageDataFromURL_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT()
-        
-        sut.loadImageData(from: url) { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
+    // MARK: - Helpers
     
-    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT()
-        
-        sut.loadImageData(from: url) { _ in }
-        sut.loadImageData(from: url) { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
+    class HTTPClientSpy {
+        let requestedURLs = [URL]()
     }
-    
-    func test_loadImageDataFromURL_deliversErrorOnClientError() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT()
-        let clientError = NSError(domain: "a client error", code: 0)
-        
-        expect(sut, toCompleteWith: .failure(clientError), when: { client.complete(with: clientError)})
-
-    }
-    
-    // MARK:- Helpers
-    
-    private func makeSUT(url: URL = URL(string: "https://any-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
-        trackForMemoryLeaks(client, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, client)
-    }
-    
-    private func expect(_ sut: RemoteFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let url = URL(string: "https://a-given-url.com")!
-        
-        let exp = expectation(description: "Waitign for load completion")
-        
-        sut.loadImageData(from: url) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-            default:
-                XCTFail("Expected \(expectedResult) got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void )]()
-        
-        var requestedURLs: [URL] {
-            return messages.map {$0.url}
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-        
-        func complete(with error: NSError, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-    }
-
 }
