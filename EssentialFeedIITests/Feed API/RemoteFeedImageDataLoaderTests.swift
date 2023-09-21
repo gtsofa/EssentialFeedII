359683237ed97a362f66b2dff7b527a5077eb0df
gtsofa <jlsnyule@yahoo.com>
2023-09-21 13:18:49 +0300

    Make a `makeSUT()` factory method to prevent future breaking changes

--- EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedIITests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -22,15 +22,13 @@ final class RemoteFeedImageDataLoader {
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
         XCTAssertEqual(client.requestedURLs, [])
     }
     
     func test_loadImageData_requestDataFromURL() {
         let url = URL(string: "https://any-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
+        let (sut, client) = makeSUT()
         
         sut.loadImageData(from: url) { _ in }
         
@@ -38,6 +36,15 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
+        
+    }
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
