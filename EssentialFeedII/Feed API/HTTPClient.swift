1a2ef737e4f6a6dc3ae7cc3b7c56faf45267ddec
gtsofa <jlsnyule@yahoo.com>
2023-09-22 12:53:44 +0300

    Add `HTTPClientTask` so clients can cancel HTTP requests

--- EssentialFeedII/Feed API/HTTPClient.swift
+++ EssentialFeedII/Feed API/HTTPClient.swift
@@ -7,11 +7,15 @@
 
 import Foundation
 
+public protocol HTTPClientTask {
+    func cancel()
+}
+
 public enum HTTPClientResult {
     case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) -> HTTPClientTask
 }
