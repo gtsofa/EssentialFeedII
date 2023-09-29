e1a54f33769e5cb73ed5e31a53aa3654ed4e5783
gtsofa <jlsnyule@yahoo.com>
2023-09-29 12:44:06 +0300

    Move `LocalFeedItem` to its own file

--- EssentialFeedII/Feed Cache/FeedStore.swift
+++ EssentialFeedII/Feed Cache/FeedStore.swift
@@ -14,17 +14,3 @@ public protocol FeedStore {
     func deleteCacheFeed(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
 }
-
-public struct LocalFeedItem: Hashable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let url: URL
-    
-    public init(id: UUID, description: String?, location: String?, url: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.url = url
-    }
-}
