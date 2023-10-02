926c7bb2ab036c8c537b6072a16b0e7a840dd462
gtsofa <jlsnyule@yahoo.com>
2023-10-02 15:18:19 +0300

    add a space

--- EssentialFeedII/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeedII/Feed Cache/LocalFeedLoader.swift
@@ -80,6 +80,7 @@ extension LocalFeedLoader {
     public func validateCache() {
         store.retrieve { [ weak self ] result in
             guard let self = self else { return }
+            
             switch result {
             case .failure:
                 self.store.deleteCacheFeed { _ in }
