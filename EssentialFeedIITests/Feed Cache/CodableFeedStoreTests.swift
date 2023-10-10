7b28373ddc1c6dae0e0f9f6e2e162b34f27ec397
gtsofa <jlsnyule@yahoo.com>
2023-10-10 14:53:29 +0300

    Extract `FailableInsertFeedStoreSpecs` checks into reusable assertions

--- EssentialFeedIITests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedIITests/Feed Cache/CodableFeedStoreTests.swift
@@ -88,23 +88,15 @@ final class CodableFeedStoreTests: XCTestCase, FailabeFeedStore {
     func test_insert_deliversErrorOnInsertionError() {
         let invalidURL = URL(string: "invalid://store-url")!
         let sut = makeSUT(storeURL: invalidURL)
-        let feed = uniqueFeedImage().local
-        let timestamp = Date()
         
-        let insertionError = insert((feed, timestamp), to: sut)
-        
-        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with error")
+        assertThatInsertDeliversErrorOnInsertionError(on: sut)
     }
     
     func test_insert_hasNoSideEffectsOnInsertionError() {
         let invalidURL = URL(string: "invalid://store-url")!
         let sut = makeSUT(storeURL: invalidURL)
-        let feed = uniqueFeedImage().local
-        let timestamp = Date()
         
-        insert((feed, timestamp), to: sut)
-        
-        expect(sut, toRetrieve: .empty)
+        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
