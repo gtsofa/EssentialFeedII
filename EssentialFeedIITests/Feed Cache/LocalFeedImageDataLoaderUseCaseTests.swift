d1d917f7c369754e4e7ad67caf07663bfca2fa70
gtsofa <jlsnyule@yahoo.com>
2023-10-21 12:20:31 +0300

    Create a `makeSUT()` factory helper method to prevent future breaking change

--- EssentialFeedIITests/Feed Cache/LocalFeedImageDataLoaderUseCaseTests.swift
+++ EssentialFeedIITests/Feed Cache/LocalFeedImageDataLoaderUseCaseTests.swift
@@ -13,9 +13,22 @@ final class LocalFeedImageDataLoader {
 
 final class LocalFeedImageDataLoaderUseCaseTests: XCTestCase {
     func test_init_doesNotMessageStoreUponCreation() {
-        let store = FeedStoreSpy()
-        _ = LocalFeedImageDataLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line )
+        return (sut, store)
+    }
+    
+    private class FeedStoreSpy {
+        var receivedMessages = [Any]()
     }
 }
