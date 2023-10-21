2cb8d77e7477f2ee785a2ac2687459004958081b
gtsofa <jlsnyule@yahoo.com>
2023-10-21 13:41:16 +0300

    `LocalFeedImageDataLoader.loadImageData()` requests store data for URL from `FeedImageDataStore`

--- EssentialFeedIITests/Feed Cache/LocalFeedImageDataLoaderUseCaseTests.swift
+++ EssentialFeedIITests/Feed Cache/LocalFeedImageDataLoaderUseCaseTests.swift
@@ -6,9 +6,27 @@
 //
 
 import XCTest
+import EssentialFeedII
 
-final class LocalFeedImageDataLoader {
-    init(store: Any) {}
+protocol FeedImageDataStore {
+    func retrieve(dataFor url: URL)
+}
+
+final class LocalFeedImageDataLoader: FeedImageDataLoader {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    
+    let store: FeedImageDataStore
+    
+    init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        store.retrieve(dataFor: url)
+        return Task()
+    }
 }
 
 final class LocalFeedImageDataLoaderUseCaseTests: XCTestCase {
@@ -18,17 +36,36 @@ final class LocalFeedImageDataLoaderUseCaseTests: XCTestCase {
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
     
+    func test_loadImageDataFromURL_requestsDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = URL(string: "https://any-url.com")!
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+    }
+    
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+        let store = StoreSpy()
         let sut = LocalFeedImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line )
         return (sut, store)
     }
     
-    private class FeedStoreSpy {
-        var receivedMessages = [Any]()
+    private class StoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
+        
+        var receivedMessages = [Message]()
+        
+        func retrieve(dataFor url: URL) {
+            receivedMessages.append(.retrieve(dataFor: url))
+        }
     }
+    
 }
